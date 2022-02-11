188ed80a21a0532fb120cd07f1ad2d0792299dbf
Jacob Howard <78132763+jah0147@users.noreply.github.com>
2022-02-11 03:49:20 -0600

    Add files via upload

---
+++ COMP 210 - Software Construction/Project 1/cmake-build-debug/CMakeFiles/Project_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_1.dir/Solution_project1.cpp.o"
+  "Project_1.exe"
+  "Project_1.pdb"
+  "libProject_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
